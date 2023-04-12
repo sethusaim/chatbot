f9f67f635ad0286207dff67cd97e0a6d458710d1
sethusaim <sethusaim@gmail.com>
2023-04-12 16:35:33 +0530

    updated code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 
-from src.components.discord_bot import client, discord_token
 from src.components.telegram_bot import bot
 from src.exception import CustomException
 
@@ -16,24 +15,5 @@ def run_telegram_bot():
         raise CustomException(e, sys)
 
 
-def run_discord_bot():
-    """
-    This function runs a Discord bot using a provided token and raises a custom exception if an error
-    occurs.
-    """
-    try:
-        client.run(discord_token)
-
-    except Exception as e:
-        raise CustomException(e, sys)
-
-
-def run_whatsapp_bot():
-    """
-    This is a Python function that runs a WhatsApp bot and catches any exceptions that occur.
-    """
-    try:
-        pass
-
-    except Exception as e:
-        raise CustomException(e, sys)
+if __name__ == "__main__":
+    run_telegram_bot()
